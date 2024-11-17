7920f04a089adc336f01b37b6025b8773b320d2c
EnzoMPP <enzo.paiva@viannasempre.com.br>
2024-11-17 20:30:10 -0300

    simplificado o sistema

--- MyStockTrack_/src/navigation/AppNavigator.js
+++ MyStockTrack_/src/navigation/AppNavigator.js
@@ -3,11 +3,8 @@ import { Image, View, StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
-import AtivosScreen from "../screens/AtivosScreen";
 import TransacoesScreen from "../screens/TransacoesScreen";
 import DepositosScreen from "../screens/DepositosScreen";
-import RentabilidadeScreen from "../screens/RentabilidadeScreen";
-import HistoricoScreen from "../screens/HistoricoScreen";
 import FavoritosScreen from "../screens/FavoritosScreen";
 import PerfilScreen from "../screens/PerfilScreen";
 import LoginScreen from "../screens/LoginScreen";
@@ -48,25 +45,6 @@ function AppTabs() {
       />
 
       <Tab.Screen
-        name="Ativos"
-        component={AtivosScreen}
-        options={{
-          tabBarIcon: ({ color, size, focused }) => (
-            <View
-              style={
-                focused ? styles.iconContainerActive : styles.iconContainer
-              }
-            >
-              <Image
-                source={require("../../assets/icons/ativos.png")}
-                style={{ width: size, height: size }}
-              />
-            </View>
-          ),
-        }}
-      />
-
-      <Tab.Screen
         name="Transações"
         component={TransacoesScreen}
         options={{
@@ -97,44 +75,6 @@ function AppTabs() {
             >
               <Image
                 source={require("../../assets/icons/depositos.png")}
-                style={{ width: size, height: size }}
-              />
-            </View>
-          ),
-        }}
-      />
-
-      <Tab.Screen
-        name="Rentabilidade"
-        component={RentabilidadeScreen}
-        options={{
-          tabBarIcon: ({ color, size, focused }) => (
-            <View
-              style={
-                focused ? styles.iconContainerActive : styles.iconContainer
-              }
-            >
-              <Image
-                source={require("../../assets/icons/rentabilidade.png")}
-                style={{ width: size, height: size }}
-              />
-            </View>
-          ),
-        }}
-      />
-
-      <Tab.Screen
-        name="Histórico"
-        component={HistoricoScreen}
-        options={{
-          tabBarIcon: ({ color, size, focused }) => (
-            <View
-              style={
-                focused ? styles.iconContainerActive : styles.iconContainer
-              }
-            >
-              <Image
-                source={require("../../assets/icons/historico.png")}
                 style={{ width: size, height: size }}
               />
             </View>
